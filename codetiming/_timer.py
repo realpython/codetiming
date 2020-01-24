c79020d8ee2d0089ed2b4fc301039e84911a1688
Geir Arne Hjelle <geirarne@gmail.com>
2020-01-24 10:52:47 +0100

    Convert unneccesary f-strings to regular literals

--- codetiming/_timer.py
+++ codetiming/_timer.py
@@ -35,14 +35,14 @@ class Timer(ContextDecorator):
     def start(self) -> None:
         """Start a new timer"""
         if self._start_time is not None:
-            raise TimerError(f"Timer is running. Use .stop() to stop it")
+            raise TimerError("Timer is running. Use .stop() to stop it")
 
         self._start_time = time.perf_counter()
 
     def stop(self) -> float:
         """Stop the timer, and report the elapsed time"""
         if self._start_time is None:
-            raise TimerError(f"Timer is not running. Use .start() to start it")
+            raise TimerError("Timer is not running. Use .start() to start it")
 
         # Calculate elapsed time
         self.last = time.perf_counter() - self._start_time
