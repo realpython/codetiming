52fe53de07009b6b960c29cbe91dcc576922680f
Geir Arne Hjelle <geirarne@gmail.com>
2020-03-03 23:37:45 +0100

    Bump version: 1.1.0 → 1.2.0

--- codetiming/__init__.py
+++ codetiming/__init__.py
@@ -25,4 +25,4 @@ You can use `codetiming.Timer` in several different ways:
 from codetiming._timer import Timer, TimerError  # noqa
 
 # Versioning is handled by bump2version
-__version__ = "1.1.0"
+__version__ = "1.2.0"
